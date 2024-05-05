db2122b4ea163e632979edb118e05e9ae8d7df3e
96ayad <167616524+96ayad@users.noreply.github.com>
2024-05-05 22:09:38 +0300

    import

--- src/components/CreateToken.tsx
+++ src/components/CreateToken.tsx
@@ -1,7 +1,7 @@
 import { FC, useCallback, useState } from 'react';
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { Keypair, PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
-import { MINT_SIZE, TOKEN_PROGRAM_ID, createInitializeMintInstruction, getMinimumBalanceForRentExemptMint, getAssociatedTokenAddress, createAssociatedTokenAccountInstruction, createMintToInstruction ,createSetAuthorityInstruction} from '@solana/spl-token';
+import { MINT_SIZE, TOKEN_PROGRAM_ID, createInitializeMintInstruction, getMinimumBalanceForRentExemptMint, getAssociatedTokenAddress, createAssociatedTokenAccountInstruction, createMintToInstruction ,createSetAuthorityInstruction ,AuthorityType } from '@solana/spl-token';
 import { createCreateMetadataAccountV3Instruction, PROGRAM_ID } from '@metaplex-foundation/mpl-token-metadata';
 
 export const CreateToken: FC = () => {
@@ -82,10 +82,8 @@ export const CreateToken: FC = () => {
           mintKeypair.publicKey, // mint acocunt || token account
           // tokenATA,
           publicKey, // current auth
-          FreezeAccount,  // authority type
-          null, 
-          // [],
-          TOKEN_PROGRAM_ID,
+          AuthorityType.FreezeAccount, // authority type
+          null
         )
 
         
